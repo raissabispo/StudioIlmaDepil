e36d19631109faeef9d0e2ed764d4cf95d9261bc
raissabispo <uamr.ieadpe1>
2024-09-17 17:39:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navbar = document.querySelector('.navbar');
+
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+
+    
+});
+
